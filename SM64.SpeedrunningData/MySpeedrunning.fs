8b09627e3c2e61a3a7a1f360a27a6ca39d4e50d1
Stachu Korick <hello@stachu.net>
2020-07-09 10:00:19 -0400

    Web stuff; and site now on sm64.net!

--- SM64.SpeedrunningData/MySpeedrunning.fs
+++ SM64.SpeedrunningData/MySpeedrunning.fs
@@ -1,10 +1,6 @@
 module SM64.MySpeedrunning
 
 open SM64.Routing
-open SM64.Courses
-open SM64.Stars
-open SM64.Speedrunner
-open SM64.Speedrunning
 
 let standardIntro =
     [ ScreenMovement TurnOnSystemAndGetToMarioFace
@@ -13,58 +9,3 @@ let standardIntro =
       CastleMovement FromFrontDoorToBobDoor
       CastleMovement FromBobDoorToBobPainting ]
 
-let mySixteenStarRoute: Segment list =
-    [ yield! standardIntro
-      
-      // star #1: BoB
-      Star (Standard BehindChainChompsGate) // note: don't *quite* like the interface here...
-      
-      // star #2-8: Whomp's
-      Star (StandardAndHundred (RedCoinsOnTheFloatingIsle, WhompsFortress))
-      Star (Standard ChipOffWhompsBlock)
-      Star (Standard ToTheTopOfTheFortress)
-      Star (Standard ShootIntoTheWildBlue)
-      Star (Standard FallOntoTheCagedIsland)
-      Star (Standard BlastAwayTheWall)
-      
-      // star #9, Bowser #1
-      BowserSegment (GetToBowserWithReds BitDW)
-      
-      // star #10, 11: SSL
-      Star (Standard InTheTalonsOfTheBigBird)
-      Star (Standard ShiningAtopThePyramid)
-      
-      // star #12: LLL
-      Star (Standard EightCoinPuzzleWith15Pieces)
-      
-      // star #13-15: HMC and toad
-      Star (ToadStar HMCToad)
-      Star (Standard AMazeingEmergencyExit)
-      Star (Standard WatchForRollingRocks)
-      
-      CastleMovement (MIPsClipStuff)
-      
-      // star #16: DDD (unlock bowser 2 pipe
-      Star (Standard BoardBowsersSub)
-      
-      // bowser 2
-      BowserSegment (JustGetToBowser BitFS)
-      
-      // todo: get from bowser 2 to blj #1
-      
-      // blj#1
-      CastleMovement (BLJ50Stairway)
-      
-      // todo: get from blj #1 to blj #2
-      
-      // blj #2
-      CastleMovement (BLJ70Stairway)
-      
-      // bowser 3, get to bowser, then fight him
-      BowserSegment (JustGetToBowser BitS)
-      BowserSegment (FightBowser BitS) ]
-
-
-let stachuSpeedrunner: Speedrunner =
-    { Name = "Stachu Korick"
-      Routes = [ SixteenStar, mySixteenStarRoute ] }
